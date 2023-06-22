822d1a0bb9a167f61dfd34ff51354581980a4b1d
AnastasiiaPavlovska <nastiapavlovska446@gmail.com>
2023-06-22 16:48:30 +0200

    Завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+var changeColorButton = document.querySelector(".change-color");
+var colorSpan = document.querySelector(".color");
+var body = document.body;
+
+changeColorButton.addEventListener("click", function() {
+  var color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+});
